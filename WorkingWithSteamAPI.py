9f492ea625b1ba32b460c407bd9bf7fdf45e32ff
Simon Sardorf <ssardorf@gmail.com>
2020-04-29 01:16:33 +0200

    Functional Steam Scraper

---
+++ WorkingWithSteamAPI.py
@@ -0,0 +1,139 @@
+import requests
+import pandas as pd
+import sys
+pd.options.mode.chained_assignment = None  # default='warn'
+api_key = 0
+r = requests.get(f"http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key={api_key}&format=json")
+
+games = r.json()["applist"]["apps"]
+
+#store.steampowered.com/appreviews/<appid>?json=1
+#https://store.steampowered.com/api/appdetails?appids=<appid>
+
+df = pd.DataFrame.from_dict(games)
+
+dfCopy = df
+testDF = df.tail(100)
+
+
+
+def getReviewInfo(appid):
+    r = requests.get(f"http://store.steampowered.com/appreviews/{appid}?json=1")
+    try:
+        review_info = r.json()["query_summary"]
+        del review_info["num_reviews"]
+        return review_info
+    except:
+        return
+
+
+def getGameInfo(appid):
+    game_info = {}
+    r = requests.get(f"https://store.steampowered.com/api/appdetails?appids={appid}")
+    success = r.json()[str(appid)]["success"]
+    try:
+        data = r.json()[str(appid)]["data"]
+    except:
+        pass
+
+    #Add success
+    game_info["success"] = success
+
+    #Add isfree
+    try:
+        is_free = data["is_free"]
+        game_info["is_free"] = is_free
+    except:
+        pass
+    #Add price data
+    try:
+        price = data["price_overview"]["final_formatted"]
+        game_info["price"] = price
+    except:
+        pass
+
+    #Add release date
+    try:
+        release_date = data["release_date"]["date"]
+        game_info["release_date"] = release_date
+    except:
+        pass
+
+    #Add genre data
+    try:
+        genres = []
+        for i in data["genres"]:
+            genres.append(i["description"])
+        genres = ", ".join(genres)
+        game_info["genres"] = genres
+    except:
+        pass
+
+    #Add category data
+    try:
+        tags = []
+        for i in data["categories"]:
+            tags.append(i["description"])
+        tags = ", ".join(tags)
+        game_info["tags"] = tags
+    except:
+        pass
+
+    #Add developers (IF MULTIPLE, THEN IT IS JOINED BY " & ")
+    try:
+        developers = data["developers"]
+        developers = " & ".join(developers)
+        game_info["developers"] = developers
+    except:
+        pass
+    #Add publishers (IF MULTIPLE, THEN IT IS JOINED BY " & ")
+    try:
+        publishers = data["publishers"]
+        publishers = " & ".join(publishers)
+        game_info["publishers"] = publishers
+    except:
+        pass
+
+    #Add short_description
+    try:
+        short_description = data["short_description"]
+        game_info["short_description"] = short_description
+    except:
+        pass
+
+    #Add metacritic score
+    try:
+        metacritic = data["metacritic"]["score"]
+        game_info["metacritic_score"] = metacritic
+    except:
+        pass
+
+    return game_info
+
+
+def addInfo(df, index, dictInfo):
+    for k,v in dictInfo.items():
+        df.at[index, k] = v
+
+    #df[index]
+
+def extract(df, csvname):
+    i = 1
+    for index, row in df.iterrows():
+        print(str(i) + " out of " + str(len(df)))
+        i = i+1
+        try:
+            review_info = getReviewInfo(row["appid"])
+            game_info = getGameInfo(row["appid"])
+            addInfo(df, index, game_info)
+            if review_info is not None:
+                addInfo(df, index, review_info)
+
+            df.to_csv(csvname+".csv")
+
+        except:
+            print(sys.exc_info()[0])
+            continue
+
+
+extract(testDF, "test")
